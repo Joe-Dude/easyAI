4587f4cfaf2cfc10f75ddde2c00270cb9c49c3f7
Joe-Dude <EEstleman2030@bcsstudent.org>
2026-06-02 16:20:07 -0400

    debug with perplexity

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
 const aiHELLO = [
-    "Hello there!",
-    "Something on ur mind?",
-    "Ready when u r!",
-    "Bonjoir!",
-    "Hola!!!"
+  "Hello there!",
+  "Something on ur mind?",
+  "Ready when u r!",
+  "Bonjoir!",
+  "Hola!!!"
 ];
 
 const more2say = [
-    "What's next?",
-    "Anything else?",
-    "Follow up!"
+  "What's next?",
+  "Anything else?",
+  "Follow up!"
 ];
 
 function say(SOMETHING) {
-    return SOMETHING[Math.floor(Math.random() * SOMETHING.length)];
+  return SOMETHING[Math.floor(Math.random() * SOMETHING.length)];
 }
 
-const textInput = document.getElementById("input")
-textInput.placeholder = say(aiHELLO)
+const textInput = document.getElementById("input");
+textInput.placeholder = say(aiHELLO);
 
-const form = document.getElementById("prompty-prompty")
+const form = document.getElementById("prompty-prompty");
 form.addEventListener("submit", function(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    textInput.value = "";
-
-    textInput.placeholder = say(more2say);
-
-    textInput.focus();
+  textInput.value = "";
+  textInput.placeholder = say(more2say);
+  textInput.focus();
 });
